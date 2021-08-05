e7e26f78faba8d7a7a74b736fa6573b5cf5371a0
Adrian Alan Pol <adrianalan.pol@gmail.com>
2021-08-05 15:32:29 +0200

    Add argparser

--- demo.py
+++ demo.py
@@ -1,10 +1,10 @@
 # Based on: kuangliu/pytorch-cifar
 
+import argparse
 import os
 import torch
 import torch.backends.cudnn as cudnn
 import torch.nn as nn
-import torch.nn.functional as F
 import torch.optim as optim
 import torchvision
 import torchvision.transforms as transforms
@@ -15,59 +15,39 @@ from utils import AverageMeter, Plotting
 from tqdm import trange
 
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
+def get_cifar10_loaders(train_batch_size=25, test_batch_size=100):
 
-# Prepare CIFAR10 dataset
-transform_train = transforms.Compose([
-    transforms.RandomCrop(32, padding=4),
-    transforms.RandomHorizontalFlip(),
-    transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465),
-                         (0.2023, 0.1994, 0.2010)),
-])
+    train_loader = torch.utils.data.DataLoader(
+        torchvision.datasets.CIFAR10(
+            root='./data',
+            train=True,
+            download=True,
+            transform=transforms.Compose([
+                transforms.RandomCrop(32, padding=4),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                transforms.Normalize((0.4914, 0.4822, 0.4465),
+                                     (0.2023, 0.1994, 0.2010)),
+                ])),
+        batch_size=train_batch_size,
+        shuffle=True,
+        num_workers=2)
 
-transform_test = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465),
-                         (0.2023, 0.1994, 0.2010)),
-])
+    test_loader = torch.utils.data.DataLoader(
+        torchvision.datasets.CIFAR10(
+            root='./data',
+            train=False,
+            download=True,
+            transform=transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Normalize((0.4914, 0.4822, 0.4465),
+                                     (0.2023, 0.1994, 0.2010)),
+                ])),
+        batch_size=test_batch_size,
+        shuffle=False,
+        num_workers=2)
 
-train_loader = torch.utils.data.DataLoader(
-    torchvision.datasets.CIFAR10(
-        root='./data',
-        train=True,
-        download=True,
-        transform=transform_train),
-    batch_size=128,
-    shuffle=True,
-    num_workers=2)
-
-test_loader = torch.utils.data.DataLoader(
-    torchvision.datasets.CIFAR10(
-        root='./data',
-        train=False,
-        download=True,
-        transform=transform_test),
-    batch_size=100,
-    shuffle=False,
-    num_workers=2)
-
-classes = ('plane', 'car', 'bird', 'cat', 'deer',
-           'dog', 'frog', 'horse', 'ship', 'truck')
-
-# Prepare the ResNet18 model
-net = ResNet18().to(device)
-if device == 'cuda':
-    net = torch.nn.DataParallel(net)
-    cudnn.benchmark = True
-
-# Training
-criterion = nn.CrossEntropyLoss().cuda()
-optimizer = optim.SGD(net.parameters(),
-                      lr=0.01,
-                      momentum=0.9,
-                      weight_decay=5e-4)
-scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
+    return train_loader, test_loader
 
 
 def run_epoch(epoch,
@@ -99,15 +79,15 @@ def run_epoch(epoch,
             prefix, epoch, loss, accuracy))
         tr.update(1)
     tr.close()
-    return loss.average, accuracy.average,
+    return loss.average, accuracy.average
 
 
-def run(epoch, train=True, fp16=False):
+def run(epoch, loader,  train=True, fp16=False):
     if train:
         if fp16:
             net.float()
         net.train()
-        loss, accuracy = run_epoch(epoch, net, train_loader)
+        loss, accuracy = run_epoch(epoch, net, loader)
     else:
         net.eval()
         if fp16:
@@ -115,41 +95,101 @@ def run(epoch, train=True, fp16=False):
         with torch.no_grad():
             loss, accuracy = run_epoch(epoch,
                                        net,
-                                       test_loader,
+                                       loader,
                                        prefix='Test',
                                        backprop=False,
                                        fp16=fp16)
     return loss, accuracy
 
 
-train_accuracy, test_accuracy = [], []
-train_loss, test_loss = [], []
+if __name__ == '__main__':
 
-fp16 = True
+    parser = argparse.ArgumentParser('Train ResNet18 on CIFAR10')
+    parser.add_argument('name',
+                        type=str,
+                        help='Model name')
+    parser.add_argument('-b',
+                        type=int,
+                        help='Training batch size',
+                        default=25,
+                        dest='batch_size')
+    parser.add_argument('-e',
+                        type=int,
+                        help='Training epochs',
+                        default=20,
+                        dest='epochs')
+    parser.add_argument('--lr',
+                        type=float,
+                        help='Learning rate',
+                        default=0.01,
+                        dest='learning_rate')
+    parser.add_argument('--momentum',
+                        type=float,
+                        help='SGD momentum',
+                        default=0.9,
+                        dest='momentum')
+    parser.add_argument('--decay',
+                        type=float,
+                        help='SGD weight decay',
+                        default=5e-4,
+                        dest='weight_decay')
+    parser.add_argument('--fp16',
+                        action="store_true",
+                        help='Inference in half precision')
+    parser.add_argument('-v', '--verbose',
+                        action="store_true",
+                        help='Output verbosity')
 
-best_accuracy = 0
+    args = parser.parse_args()
 
-for epoch in range(1, 20):
+    # Creating directories
+    for d in ['models', 'plots']:
+        if not os.path.isdir(d):
+            os.mkdir(d)
 
-    loss, accuracy = run(epoch, train=True, fp16=fp16)
-    train_loss.append(loss)
-    train_accuracy.append(accuracy)
+    # Prepare CIFAR10 dataset
+    train_loader, test_loader = get_cifar10_loaders(
+        train_batch_size=args.batch_size)
 
-    loss, accuracy = run(epoch, train=False, fp16=fp16)
-    test_loss.append(loss)
-    test_accuracy.append(accuracy)
+    # Prepare the ResNet18 model
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    net = ResNet18().to(device)
+    if device == 'cuda':
+        cudnn.benchmark = True
+        net = torch.nn.DataParallel(net)
 
-    if accuracy > best_accuracy:
-        state = {
-            'accuracy': accuracy,
-            'epoch': epoch,
-            'net': net.state_dict()
-        }
-        if not os.path.isdir('models'):
-            os.mkdir('models')
-        torch.save(net.state_dict(), './models/checkpoint.pth')
-        best_accuracy = accuracy
-    scheduler.step()
+    # Training
+    criterion = nn.CrossEntropyLoss().to(device)
+    optimizer = optim.SGD(net.parameters(),
+                          lr=args.learning_rate,
+                          momentum=args.momentum,
+                          weight_decay=args.weight_decay)
+    scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
 
-plot = Plotting('plots/results.png')
-plot.draw(train_loss, train_accuracy, test_loss, test_accuracy)
+    train_accuracy, test_accuracy = [], []
+    train_loss, test_loss = [], []
+
+    best_accuracy = 0
+
+    for epoch in range(1, args.epochs+1):
+
+        loss, accuracy = run(epoch, train_loader, train=True, fp16=args.fp16)
+        train_loss.append(loss)
+        train_accuracy.append(accuracy)
+
+        loss, accuracy = run(epoch, test_loader, train=False, fp16=args.fp16)
+        test_loss.append(loss)
+        test_accuracy.append(accuracy)
+
+        if accuracy > best_accuracy:
+            state = {
+                'accuracy': accuracy,
+                'epoch': epoch,
+                'net': net.state_dict()
+            }
+            torch.save(net.state_dict(), 'models/{}.pth'.format(args.name))
+            best_accuracy = accuracy
+        scheduler.step()
+
+    plot = Plotting('plots/{}-results.png'.format(args.name))
+    plot.draw(train_loss, train_accuracy, test_loss, test_accuracy)
